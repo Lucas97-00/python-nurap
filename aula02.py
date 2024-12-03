062462aaf85491601789aa3829e6dc9cd99fc4e4
Lucas Nascimento <lucasnascimento1322@gmail.com>
2024-12-03 09:44:18 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,8 @@
+print('hoje o dia ta nublado')
+
+nome_usuario = 'lucas nascimento' # tipo -> str = string 
+idade_usuario = 17 # tipo -> int = integer 
+altura_usuario = 1,65 # tipo -> float = ponto flutuante
+maior_idade = False # tipo -> bool = boolean 
+
+print (('seja bem-vindo'), nome_usuario, ('sua idade é'), idade_usuario, ('anos, você tem'), altura_usuario, ("metros. você é maior de idade?"), maior_idade)
